548beb7cdf1c3e913d9c44f53478da03694e7061
widal001 <williamdaly422@gmail.com>
2021-05-06 17:40:01 -0400

    Adds populate to run.py

--- run.py
+++ run.py
@@ -1,13 +1,15 @@
 from app import create_app
 from app.api.models import db
+from tests.populate_db import populate
 
 
 def create_local_app():
     app = create_app()
 
-    # with app.app_context():
-        # db.drop_all()  # Drop existing tables
-        # db.create_all()  # Create sql tables for our data models
+    with app.app_context():
+        db.drop_all()  # Drop existing tables
+        db.create_all()  # Create sql tables for our data models
+        populate()  # populates a local db with test data
 
     return app
 
